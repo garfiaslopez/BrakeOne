d470bbaccc643846bf6a4106f17bf3187fcadd9f
Jose Garfias Lopez <garfiaslopez@hotmail.com>
2019-04-15 01:23:16 -0500

    fix bakcup cron

--- service/config/scripts/mongo_backup.sh
+++ service/config/scripts/mongo_backup.sh
@@ -13,7 +13,7 @@ echo "Dumping MongoDB $DB database to compressed archive"
 mongodump -h localhost --port 18509 --username BrakeOneService --password bmnqdQUObC4AS11i --authenticationDatabase $DB -d $DB --archive=$HOME/backup/tmp_dump.gz --gzip
 
 echo "Copying compressed archive to DigitalOcean Space: $SPACE_NAME"
-s3cmd put $HOME/backup/tmp_dump.gz s3://$SPACE_NAME/$BACKUP_NAME
+/usr/local/bin/s3cmd put $HOME/backup/tmp_dump.gz s3://$SPACE_NAME/$BACKUP_NAME
 
 echo "Cleaning up compressed archive"
 rm $HOME/backup/tmp_dump.gz
