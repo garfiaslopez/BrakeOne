3c6812f51c9e0a9d4d79dcb800d0cf9011e5a855
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2021-01-05 10:39:27 -0600

    Pruebas

--- service/controllers/customFunctions.js
+++ service/controllers/customFunctions.js
@@ -14,6 +14,7 @@ module.exports =  {
         if (req.body.brand && req.body.quantity_percent) { // update by brand 
             Filter.brand = req.body.brand;
 
+            Products.key_id = 'ANGEL';
             NewProperties.stock = 1;
            
             objectModel.update(
