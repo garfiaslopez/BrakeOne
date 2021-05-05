b6b42c151c0f55da2f6e6ca77015499cedb2fc8d
= <=>
2021-05-05 00:59:58 -0500

    Update QuotationRecipe

--- web_admin/src/components/PrinterRecipes/QuotationRecipe/QuotationRecipe.jsx
+++ web_admin/src/components/PrinterRecipes/QuotationRecipe/QuotationRecipe.jsx
@@ -9,21 +9,6 @@ import NumeroALetras from '../../../helpers/number_to_letter';
 
 const round2 = (number) => (Math.round(number * 100) / 100);
 
-let fontSize = 14
-
-function renderText (text) {
-    let color = 'black';
-    return ({
-        children: <p style={{color, fontSize}}>{text}</p>,
-    });
-}
-
-function renderRowNumber (text, record) {
-    return ({
-        children: <p style={{fontSize}}>${String(round2(text ? text : 0)).replace(/\B(?=(\d{4})+(?!\d))/g, ',')}</p>,
-    });
-}
-
 const propTypes = {
     client: PropTypes.object,
     vehicle: PropTypes.object,
@@ -31,47 +16,39 @@ const propTypes = {
 };
 
 const defaultProps = {
-    
     client: {
         title: 'Nombre',
         dataIndex: 'name',
         key: 'name',
-        width: "10%",
     },
    
     quotationColumns: [
     {
         title: 'Clave',
         dataIndex: 'key_id',
-        key: 'key_id',    
-        width: "8%",   
-        render: renderText
+        key: 'key_id',       
     }, 
     {
         title: 'Concepto',
         dataIndex: 'description',
         key: 'description',
-        width: "25%",
-        render: renderText
+        render: RenderRows.renderRowTextTruncateDesc,
     }, 
     {
         title: 'Cantidad',
         dataIndex: 'quantity',
         key: 'quantity',
-        width: "8%",
-        render: renderText
+        render: RenderRows.renderRowQuantity,
     },{
         title: 'Precio',
-        width: "8%",
         dataIndex: 'price',
-        key: 'price', 
-        render: renderRowNumber
+        key: 'price',
+        render: RenderRows.renderRowNumber,
     }, {
         title: 'Importe',
         dataIndex: 'total',
         key: 'total',
-        width: "8%",
-        render: renderRowNumber
+        render: RenderRows.renderRowNumber,
     }]
 };
 
@@ -98,51 +75,53 @@ const QuotationRecipe = ({
     <section className="quotation-recipe">
         <header className="">
             <img className="imgLogo" src="/images/BrakeOneBrembo.png" />
-            <b><p className='rem'>Remisión</p></b>
         </header>
         
         {/* Title */}
-        
+        <b><p className='title'>Remision</p></b>
 
         {/* Date */}
-        <body>
-        <p className="date">
-           <b>Fecha:</b> {moment().format('DD-MM-YYYY')}
+        <p className="quotation-recipe__date">
+           <b>Fecha:</b> {moment().format('MM-DD-YYYY')}
         </p>
         {/* Client Name */}
-        <ul className="recipe__flex-container_serv">
-        <p class="text_gar"><b>CLIENTE:</b> {client}</p>
-        <p class="text_gar"><b>TEL: </b> {client_phone}</p><br></br>
-        <p class="text_gar"><b>Direccion: </b> 
+        <ul className="recipe__flex-container">
+        <p><b>CLIENTE:</b> {client}</p>
+        <p><b>TEL: </b> {client_phone}</p><br></br>
+        <p><b>Direccion: </b> 
        {client_address_city} {client_address_country} C.P. {client_address_cp} {client_address_state}</p>
-        </ul>        
+        </ul>
+
+        <p>Por este medio me permito presentar a su amable consideración el presupuesto para la reparación de la siguiente unidad:</p>
        
-        <ul className="recipe__flex-container_serv">           
+        <ul className="recipe__flex-container">
+           
             <li>
                 <h3 className="recipe__item__title">Marca</h3>
-                <p class="text_gar">{vehicle.brand}</p>
+                <span>{vehicle.brand}</span>
             </li>
             <li>
                 <h3 className="recipe__item__title">Modelo</h3>
-                <p class="text_gar">{vehicle.model}</p>
+                <span>{vehicle.model}</span>
             </li>
             <li>
                 <h3 className="recipe__item__title">Año</h3>
-                <p class="text_gar">{vehicle.year}</p>
+                <span>{vehicle.year}</span>
             </li>
             <li>
                 <h3 className="recipe__item__title">Color</h3>
-                <p class="text_gar">{vehicle.color}</p>
+                <span>{vehicle.color}</span>
             </li>
             <li>
                 <h3 className="recipe__item__title">Placas</h3>
-                <p class="text_gar">{vehicle.plates}</p>
+                <span>{vehicle.plates}</span>
             </li>
             <li>
                 <h3 className="recipe__item__title">Kms</h3>
-                <p class="text_gar">{vehicle.kms}</p>
+                <span>{vehicle.kms}</span>
             </li>
-        </ul>       
+        </ul>
+        <p className="Detalle">Detalle de mano de obra y refacciones:</p>
         <Table
             size="small"
             indentSize={0}
@@ -150,72 +129,71 @@ const QuotationRecipe = ({
             dataSource={sellItems}
             columns={quotationColumns}
         />
-        <ul className="recipe__flex-container_serv">
+        <ul className="recipe__flex-container">
             <li>
                 <h3 className="recipe__item__title">
                     Importe con letra
                 </h3>
                 <span className="importLetra" >
-                    <p class="text_gar">{NumeroALetras(totalNumber) } 00/100 M.N.</p>
+                    {NumeroALetras(totalNumber) } 00/100 M.N.
                 </span>
             </li>
             <li>
                 <h3 className="recipe__item__title">
                     Total
                 </h3>
-                <span className="text_gar">
+                <span className="">
                     ${String(round2(totalNumber ? totalNumber : 0)).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                 </span>
             </li>
         </ul>
         {/* <p>Agradeciendo la atención que sirva brindar a la presente y en espera de su favorable respuesta, quedo de usted.</p> */}
         {/* <h2>Atentamente</h2> */}
-        <p class="text_gar">GARANTIA RECTIFICADO DISCOS: 5 DIAS, GARANTIA BALATAS: 30 DIAS O KM 1,000 LO QUE OCURRA PRIMERO, SOBRE DEFECTO DE FABRICACION, RECOMENDACIÓN: NO ABUSE DE LOS FRENOS DURANTE LOS PRIMEROS KM 200-300</p>
-        </body>
+        <p className="Detalle">GARANTIA RECTIFICADO DISCOS: 5 DIAS, GARANTIA BALATAS: 30 DIAS O KM 1,000 LO QUE OCURRA PRIMERO, SOBRE DEFECTO DE FABRICACION, RECOMENDACIÓN: NO ABUSE DE LOS FRENOS DURANTE LOS PRIMEROS KM 200-300</p>
+        <footer className="recipe__footer">
+
         <footer className="sell-ticket__footer">                  
           
-                <ul className="recipe__flex-container_serv">
-                    <li>
-                        <h3 className="recipe__item__title">
-                           <p class="text_gar">Califica nuestro servicio</p>
-                           <p class="text_gar">Escaneando el codigo QR </p>
-                           <p class="flecha">🢃</p> 
-                        </h3>
-                        <span className="importLetra" >
-                        <img
-                            className="sell-ticket__QR_Services"
-                            src="/images/QRComents.png"
-                        />
-                        </span>
-                    </li>
-                    <li>
-                        <h3 className="recipe__item__title">
-                            <p class="text_cond">▢ Recibí en buenas condiciones el vehículo<br></br><br></br>
-                            ▢ Falto algo de tu vehiculo?<br></br><br></br>
-                                
-                                Anotar faltantes: </p><br></br><br></br>
+          <ul className="recipe__flex-container_serv">
+              <li>
+                  <h3 className="recipe__item__title">
+                     <p class="text_gar">Califica nuestro servicio</p>
+                     <p class="text_gar">Escaneando el codigo QR </p>
+                     <p class="flecha">🢃</p> 
+                  </h3>
+                  <span className="importLetra" >
+                  <img
+                      className="sell-ticket__QR_Services"
+                      src="/images/QRComents.png"
+                  />
+                  </span>
+              </li>
+              <li>
+                  <h3 className="recipe__item__title">
+                      <p class="text_cond">▢ Recibí en buenas condiciones el vehículo<br></br><br></br>
+                      ▢ Falto algo de tu vehiculo?<br></br><br></br>
+                          
+                          Anotar faltantes: </p><br></br><br></br>
 
-                        </h3>                       
-                    </li>
-                    <li>
-                        <h3 className="recipe__item__title">
-                         
-                        </h3><br></br><br></br><br></br><br></br>
-                        <span className="text_firm">
-                           ___________________________
-                        </span>
-                        <p class="firm">Firma</p>
-                    </li>
-                </ul>
-        </footer><br></br><br></br>
-
-
-        <footer className="recipe__footer">
-            <p class="text_gar">Saratoga #313-C,
+                  </h3>                       
+              </li>
+              <li>
+                  <h3 className="recipe__item__title">
+                   
+                  </h3><br></br><br></br><br></br><br></br>
+                  <span className="text_firm">
+                     ___________________________
+                  </span>
+                  <p class="firm">Firma</p>
+              </li>
+          </ul>
+  </footer><br></br><br></br>
+  
+            <p>Saratoga #313-C,
                 Col. Portales Norte
                 C.p. 03300
-                Alcaldía Benito Juárez, CDMX.</p>
-            <p class="text_gar">Tels. (55) 4563 – 2063
+A               lcaldía Benito Juárez, CDMX.</p>
+            <p>Tels. (55) 4563 – 2063
                     (55) 6840 – 2850
                     (55) 7653 – 6116
                     (55) 5273 – 3450</p>
