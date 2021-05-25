8e1ae18cff5bba484a848973552dd86e79f572dd
= <=>
2021-05-25 14:24:10 -0500

    Porcent update

--- service/controllers/customFunctions.js
+++ service/controllers/customFunctions.js
@@ -172,7 +172,7 @@ module.exports =  {
         if (req.body.brand && req.body.quantity_percent) { // update by brand 
             Filter.brand = req.body.brand;            
 
-            const multiplier = (Number(req.body.quantity_percent) / 100) + 1;
+            const multiplier = (Number(req.body.quantity_percent) / 100);
             NewProperties.price =  Math.round(multiplier);
             NewProperties.price_credit_workshop =  Math.round(multiplier);
             NewProperties.price_public =  Math.round(multiplier);
