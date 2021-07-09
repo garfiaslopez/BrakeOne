ac5abaaa3cec9bbc806bdd415566f2d6efe7c5ea
= <=>
2021-07-09 00:48:48 -0500

    Update CRUDController

--- service/controllers/CRUDController.js
+++ service/controllers/CRUDController.js
@@ -129,7 +129,8 @@ module.exports = (method, model) => {
         }
         if (req.body.search_text != undefined) { 
             console.log('Search Text: ', req.body.search_text);                       
-            Filter['$text'] = { '$search': + "`\`" + req.body.search_text + + "`\`"};            
+            Filter['$text'] = { '$search': + "`\`" + req.body.search_text + + "`\`"};    
+            console.log(Filter);        
         };       
         
         /*Filtros de busquedas*/
