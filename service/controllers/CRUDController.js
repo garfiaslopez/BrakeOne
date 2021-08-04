f5371c6ffd3a137cd4ab8fd6cf2bc01a16891f93
= <=>
2021-08-04 17:07:53 -0500

    Update limit documents

--- service/controllers/CRUDController.js
+++ service/controllers/CRUDController.js
@@ -102,7 +102,7 @@ module.exports = (method, model) => {
     let Search = (req, res, next) => {    
         var Paginator = {
             page: 1,//Pagina
-            limit: 500000,//Limite de la pagina
+            limit: 50,//Limite de la pagina
             sort: {
                 created: -1, // desc
             }
